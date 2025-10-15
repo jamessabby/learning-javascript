ee6f888507a75b11e6066acd6ee09018356ca4c0
James Sabio <jamesjacobsabio232@gmail.com>
2025-10-15 13:34:40 +0800

    Initial commit for practice files

---
+++ functions.js
@@ -0,0 +1,97 @@
+/*
+
+function favoriteAnimal(animal) {
+ return animal + " is my favorite animal"; 
+}
+
+const message = favoriteAnimal('Lion');
+
+console.log(favoriteAnimal('Lion'));
+
+function random(number) {
+  return Math.floor(Math.random() * number);       // it doesn't require any parameters
+};
+
+console.log(random(4))
+
+*/
+
+/*
+==========================
+INVOKING FUNCTIONS
+=========================
+*/
+
+// function myFunction() {
+//   alert('Hello nigga');
+// }
+
+// myFunction();
+
+
+/*
+==========================
+FUNCTION PARAMETERS
+=========================
+*/
+
+
+// const myText = "I am a string";
+// console.log(myText);
+
+// const newText = myText.replace("string", "sausage");
+// console.log(newText);
+
+/*
+==========================
+OPTIONAL PARAMETERS
+=========================
+*/
+
+// you don't have to specify them
+
+// const myArray = ['I', 'love', 'my', 'girlfriend'];
+
+// const madeAString = myArray.join(" ");
+
+// console.log(madeAString);
+
+// const madeAnotherString = myArray.join();
+
+// console.log(madeAnotherString);
+
+// // If no parameter is included to specify a joining/delimiting character, a comma is used by default.
+
+/*
+==========================
+DEFAULT PARAMETERS
+=========================
+*/
+
+// If you're writing a function and want to support optional parameters, you can specify default values by adding = after the name of the parameter, followed by the default value
+
+// function hello(name = 'Sab') {
+//   console.log(`Hello ${name}!`)
+// };
+
+// hello('James');
+// hello();
+
+/*
+==========================
+ANONYMOUS FUNCTIONS
+=========================
+*/
+
+(function () {
+  alert("hello");
+});
+
+
+
+
+
+
+
+
+
---
+++ variables-operators.js
@@ -0,0 +1,40 @@
+let first_name = "James"
+let surname = "Sabio"
+
+// console.log(first_name);
+// console.log(surname);
+
+
+// let age = 11;
+// console.log(age);
+
+// age = 19;
+
+// console.log(age);
+
+
+// const pi = 3.14;
+
+// // pi = 10;
+
+// console.log(pi);
+
+// console.log(23+97);
+// console.log(25+31+34+67+89+45);
+
+// let a = 10;
+// console.log(a);
+
+// a = 20;
+
+// console.log(a);
+// console.log(a * 7);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage);
+
+
+
