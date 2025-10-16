e288dd97858b9aaf4278e60cbbef6ff6a9a3cae4
James Sabio <jamesjacobsabio232@gmail.com>
2025-10-17 00:06:43 +0800

    Solved fizzbuzz problem

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+let userInput = parseInt(prompt("Enter any number: "));
+
+for (i=1; i <= userInput; i++) {
+  if (i % 3 === 0 && i % 5 != 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0 && i % 3 != 0) {
+    console.log("buzz");
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log('fizzbuzz');
+  } else {
+    console.log(i);
+  }
+}
